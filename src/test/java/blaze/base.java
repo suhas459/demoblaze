c01c728718d152b0e8fd032d363e9340b10397fc
suhas <suhaspowar2856@gmail.com>
2023-03-08 18:29:08 +0530

    addd

--- src/test/java/blaze/base.java
+++ src/test/java/blaze/base.java
@@ -7,13 +7,21 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class base {
 protected	WebDriver driver;
-	public void launchTheBrowser() {
+	public void launchTheBrowser() throws InterruptedException {
 		
 		WebDriverManager.chromedriver().setup();
 		
 driver= new ChromeDriver();
 		driver.navigate().to("https://www.demoblaze.com/");
 		driver.manage().window().maximize();
+		
+		
+		
+		Thread.sleep(1000);
+		
+		
+		
+		
 		
 	}
 	
